b1c6a1be8a301621af9e397a04f2de69636708f6
Cody Alvarez <cody_alvarez19@outlook.com>
2021-03-26 02:36:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,10 @@
 class CashRegister
+  attr_accessor :total, :discount, :price, :items
 
-  def initialize
+  def initialize(discount = 0)
     @total = 0
+    @discount = discount 
+    @items = []
   end
 
 end
